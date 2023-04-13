dfa2d9bec8a498593d5a7b434ccd1f3a710e0114
Shaun McDonogh <shaun@Shauns-MacBook-Pro.local>
2023-04-13 12:53:49 +0100

    first commit

---
+++ hardhat.config.ts
@@ -0,0 +1,24 @@
+import { HardhatUserConfig } from "hardhat/config";
+import dotenv from "dotenv";
+dotenv.config();
+
+/**
+ * Run as: npx hardhat node --port 8700
+ * This is because GETH is already listening on 8545
+ */
+
+const config: HardhatUserConfig = {
+  solidity: {
+    version: "0.8.18", // Replace with your desired Solidity compiler version
+  },
+  // defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.PROVIDER_URL!,
+      },
+    },
+  },
+};
+
+export default config;
