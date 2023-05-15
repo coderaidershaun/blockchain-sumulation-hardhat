676d322982adf70bb1e69d2d5baea0d9caf2b09a
Ubuntu <ubuntu@ns3227673.ip-57-128-140.eu>
2023-05-15 15:20:58 +0000

    trace_transaction

--- scripts/debug.ts
+++ scripts/debug.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 /**
-  Listens to main live blockchain and gets current block number
+  Listens to main live blockchain and calls debug_traceTransaction
   Used to test connectivity to Geth node 8545
 */
 
@@ -23,6 +23,28 @@ async function main() {
   const address = "0x0d09aEC2D10F396fB59482644708CBd353798b87";
   const balance = await provider.getBalance(address);
   console.log("Balance of address:", ethers.utils.formatEther(balance));
+
+  // Debug a recent transaction
+  const block = await provider.getBlockWithTransactions(blockNumber);
+  if (block.transactions.length > 0) {
+
+    const recentTransaction = block.transactions[0];
+
+    // Trace transaction
+    const trace = await provider.send("debug_traceTransaction", [
+      recentTransaction.hash,
+    ]);
+
+    // // Now get the debug_traceTransaction
+    // const trace = await axios.post('http://127.0.0.1:8700', {
+    //   jsonrpc: '2.0',
+    //   id: 1,
+    //   method: 'debug_traceTransaction',
+    //   params: [txReceipt.transactionHash],
+    // });
+
+    console.log("trace");
+  }
 }
 
 // MAIN Entrypoint
